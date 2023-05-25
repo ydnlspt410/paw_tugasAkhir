e6ab8b81b4bab837d9ed59188851330f3c662f6c
Aaz Zazri Nugraha <azzazry120@gmail>
2023-05-25 21:05:12 +0700

    menambahkan mysql

--- app.py
+++ app.py
@@ -1,4 +1,32 @@
-a = 0
-if a <= 10:
-    print('Angka ke-', a)
-    a = a + 1
+from flask import Flask, render_template, request
+import mysql.connector
+
+app = Flask(__name__)
+
+# Koneksi ke database MySQL
+conn = mysql.connector.connect(
+    host="localhost", user="root", password="", database="transaksi"
+)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        nama_barang = request.form["nama_barang"]
+        jenis_transaksi = request.form["jenis_transaksi"]
+        jumlah = request.form["jumlah"]
+
+        # Menyimpan data barang masuk atau keluar ke database
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO transaksi (nama_barang, jenis_transaksi, jumlah) VALUES (%s, %s, %s)",
+            (nama_barang, jenis_transaksi, jumlah),
+        )
+        conn.commit()
+        cursor.close()
+
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
