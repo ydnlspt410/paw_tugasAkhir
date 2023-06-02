dcbe4240a67f66572f89edcb8069904e8e749452
Yongki Danil Saputra <yongkidanilsaputra@gmail.com>
2023-06-02 19:23:43 +0700

    fungsi request belum selesai

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
+from models import InventarisasiBesi as db
+import mysql.connector
+
 
 app = Flask(__name__)
+app.secret_key = '123456789'
 
+@app.route('/')
+def homepage():
+    return render_template('homepage.html')
 
-@app.route("/")
-def index():
+# fungsi login
+@app.route("/login", methods=["POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM t_kasir WHERE username = %s AND password = %s", (username, password))
+        user = cursor.fetchone()
+
+        if user:
+            session["username"] = username
+            return redirect("/")
+        else:
+            return render_template("login.html", error="Username atau password salah")
+
+    return render_template("login.html")
+
+#fungsi registrasi 
+@app.route('/registrasi', methods=['GET', 'POST'])
+def registrasi():
+    if request
+@app.route("/dash-admin")
+def dash_admin ():
     return render_template("index.html")
 
 
